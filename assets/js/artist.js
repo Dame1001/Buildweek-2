8618bd6fe7754cb90ad9774e27218e1b1d29900c
francescopasta <pastore.francesco01@gmail.com>
2022-12-14 11:19:23 +0000

    francesco_artist_funzxione

--- assets/js/artist.js
+++ assets/js/artist.js
@@ -1,30 +1,31 @@
 window.addEventListener('load', caricaContenuto);
 function caricaContenuto() {
     const div = document.querySelector('tbody');
-    const prof = document.querySelector('#band_profile')
-    const sup = document.querySelector('#sez_sup')
-    
-    fetch('https://striveschool-api.herokuapp.com/api/deezer/artist/1').then(function (response) {
-        return response.json();
-    }).then(function (json) {
-        let band = json;
-        sup.innerHTML = `
-            <div class='d-flex'>
-                <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="#00f" class="bi bi-patch-check-fill me-2 mt-1" viewBox="0 0 16 16">
-                    <path d="M10.067.87a2.89 2.89 0 0 0-4.134 0l-.622.638-.89-.011a2.89 2.89 0 0 0-2.924 2.924l.01.89-.636.622a2.89 2.89 0 0 0 0 4.134l.637.622-.011.89a2.89 2.89 0 0 0 2.924 2.924l.89-.01.622.636a2.89 2.89 0 0 0 4.134 0l.622-.637.89.011a2.89 2.89 0 0 0 2.924-2.924l-.01-.89.636-.622a2.89 2.89 0 0 0 0-4.134l-.637-.622.011-.89a2.89 2.89 0 0 0-2.924-2.924l-.89.01-.622-.636zm.287 5.984-3 3a.5.5 0 0 1-.708 0l-1.5-1.5a.5.5 0 1 1 .708-.708L7 8.793l2.646-2.647a.5.5 0 0 1 .708.708z"/>
-                </svg>
-                <p class='align-self-center'>Artista verificato</p>
-            </div>
-            <h1>${band.name}</h1>
-            <p>${band.nb_fan} ascoltatori seriali</p>
-        `
-        sup.style.background = `url(${band.picture_xl})`;
-    });
+    const prof = document.querySelector('#band_profile');
+    const sup = document.querySelector('#sez_sup');
 
-    fetch('https://striveschool-api.herokuapp.com/api/deezer/artist/1/top?limit=50').then(function (response) {
-        return response.json();
-    }).then(function (json) {
-        let album = json;
+    fetch('https://striveschool-api.herokuapp.com/api/deezer/artist/1')
+    .then(function(response) { 
+    return response.json()
+    })
+    .then(function(band) {   
+    sup.innerHTML = `
+        <div class='d-flex'>
+            <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="#00f" class="bi bi-patch-check-fill me-2 mt-1" viewBox="0 0 16 16">
+                <path d="M10.067.87a2.89 2.89 0 0 0-4.134 0l-.622.638-.89-.011a2.89 2.89 0 0 0-2.924 2.924l.01.89-.636.622a2.89 2.89 0 0 0 0 4.134l.637.622-.011.89a2.89 2.89 0 0 0 2.924 2.924l.89-.01.622.636a2.89 2.89 0 0 0 4.134 0l.622-.637.89.011a2.89 2.89 0 0 0 2.924-2.924l-.01-.89.636-.622a2.89 2.89 0 0 0 0-4.134l-.637-.622.011-.89a2.89 2.89 0 0 0-2.924-2.924l-.89.01-.622-.636zm.287 5.984-3 3a.5.5 0 0 1-.708 0l-1.5-1.5a.5.5 0 1 1 .708-.708L7 8.793l2.646-2.647a.5.5 0 0 1 .708.708z"/>
+            </svg>
+            <p class='align-self-center'>Artista verificato</p>
+        </div>
+        <h1>${band.name}</h1>
+        <p>${band.nb_fan} ascoltatori seriali</p>
+    `
+    sup.style.background = `url(${band.picture_xl})`;
+    return fetch(band.tracklist)
+    })
+    .then(function(response) { 
+    return response.json(); 
+    })
+    .then(function(album) {
         for (let i = 0; i < album.data.length; i++) {
             div.innerHTML +=
                 `<tr>
@@ -34,13 +35,16 @@ function caricaContenuto() {
                     <td class="text-end mostra_tabella"><p class="m-0">${album.data[i].rank}</p></td>
                     <td class="text-end mostra_tabella"><p class="m-0">${album.data[i].duration}</p></td>
                 </tr>`
-        }
+        };
         prof.innerHTML = `
             <tr>
                 <td><img src="${album.data[0].contributors[0].picture}" alt="band_profile" class="rounded-circle"></td>
                 <td><h4>Hai messo Mi piace a ${Math.floor(Math.random()*21)} Brani</h4><p>Di  ${album.data[0].artist.name}</p></td>
             </tr>
-        `
+        `;
+    })
+    .catch(function(error) { 
+    console.log('Requestfailed', error) 
     });
 }
 
