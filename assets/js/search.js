d2a82815b23e710e46e124a7ff8885ce0327ca03
francescopasta <pastore.francesco01@gmail.com>
2022-12-16 12:54:16 +0000

    francesco_16/12 lalala

--- assets/js/search.js
+++ assets/js/search.js
@@ -21,7 +21,7 @@ function chiamaJson(numero) {
         {
             stampa.innerHTML +=`
             <div class="col-6 col-lg-3 my-5">
-            <div class="card d-flex flex-row flex-lg-column justify-content-center align-items-center manina h-100">
+            <div class="card d-flex flex-column justify-content-center align-items-center manina h-100">
             <img src="${album.data[i].album.cover_medium}" class="card-img-top mb-1" alt="#" onclick="getId(${album.data[i].album.id})">
             <h5 class="card-title w-75 text-truncate text-center" onclick="getId(${album.data[i].album.id})">${album.data[i].title}</h5>
             <p class="card-text w-75 text-truncate pb-3 text-center" onclick="getId(${album.data[i].album.id})">${album.data[i].album.title}</p>
